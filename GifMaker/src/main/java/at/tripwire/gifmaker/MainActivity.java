d05e30b260716e4348d5a12b4bd6a51ebcfc4ea6
Max Holzleitner <max.holzleitner@instant-it.at>
2014-05-06 13:33:48 +0200

    working main activity

--- GifMaker/src/main/java/at/tripwire/gifmaker/MainActivity.java
+++ GifMaker/src/main/java/at/tripwire/gifmaker/MainActivity.java
@@ -1,36 +1,56 @@
 package at.tripwire.gifmaker;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final int CAMERA_REQUEST = 1234;
+
+    private Button captureButton;
+
+    private Button loadButton;
+
+    private Button createGifButton;
+
+    private LinearLayout thumbnailLayout;
+
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        captureButton = (Button) findViewById(R.id.capture);
+        loadButton = (Button) findViewById(R.id.load);
+        createGifButton = (Button) findViewById(R.id.createButton);
+
+        thumbnailLayout = (LinearLayout) findViewById(R.id.thumbnailLayout);
+
+        image = (ImageView) findViewById(R.id.image);
+
+        captureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            image.setImageBitmap(photo);
         }
-        return super.onOptionsItemSelected(item);
     }
 }
