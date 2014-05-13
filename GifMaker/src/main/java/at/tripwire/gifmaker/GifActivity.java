0140e5ff215662d71396ec6ce96a6ed625c28ed4
Melkizedek <martin_reif@yahoo.com>
2014-05-13 15:12:49 +0200

    GifActivity

--- GifMaker/src/main/java/at/tripwire/gifmaker/GifActivity.java
+++ GifMaker/src/main/java/at/tripwire/gifmaker/GifActivity.java
@@ -1,18 +1,35 @@
 package at.tripwire.gifmaker;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Movie;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class GifActivity extends ActionBarActivity {
+
+    private byte[] gif;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gif);
 
-        byte[] gif = getIntent().getByteArrayExtra("data");
+
+
+        gif = getIntent().getByteArrayExtra("data");
         // do something with this byte array
+
+        setContentView(new GIFView(this));
 
         writeFile(gif);
     }
@@ -25,6 +42,39 @@ public class GifActivity extends ActionBarActivity {
             outStream.close();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private class GIFView extends View {
+
+        Movie movie, movie1;
+        long moviestart;
+
+        public GIFView(Context context) {
+            super(context);
+
+            movie = Movie.decodeByteArray(gif, 0, gif.length);
+            System.out.println("\tmoviesHeight=" + movie.height());
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+
+            canvas.drawColor(Color.WHITE);
+            super.onDraw(canvas);
+            long now=android.os.SystemClock.uptimeMillis();
+            System.out.println("now="+now);
+            if (moviestart == 0) { // first time
+                moviestart = now;
+
+            }
+            System.out.println("\tmoviestart="+moviestart);
+            System.out.println("\tmoviesduration="+movie.duration());
+            int relTime = (int)((now - moviestart) % movie.duration()) ;
+            System.out.println("time="+relTime+"\treltime="+movie.duration());
+            movie.setTime(relTime);
+            movie.draw(canvas,this.getWidth()/2-20,this.getHeight()/2-40);
+            this.invalidate();
         }
     }
 }
