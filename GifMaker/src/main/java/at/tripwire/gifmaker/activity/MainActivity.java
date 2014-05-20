f9d2367c57297bed7e770e0590c7969cf69f0468
Melkizedek <martin_reif@yahoo.com>
2014-05-20 10:21:59 +0200

    SettingsActivity created

--- GifMaker/src/main/java/at/tripwire/gifmaker/activity/MainActivity.java
+++ GifMaker/src/main/java/at/tripwire/gifmaker/activity/MainActivity.java
@@ -10,6 +10,9 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -124,6 +127,27 @@ public class MainActivity extends ActionBarActivity {
         });
 
         refreshUi();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actionbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // start SettingsActivity
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void refreshUi() {
---
+++ GifMaker/src/main/java/at/tripwire/gifmaker/activity/SettingsActivity.java
@@ -0,0 +1,20 @@
+package at.tripwire.gifmaker.activity;
+
+import android.preference.PreferenceActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import at.tripwire.gifmaker.R;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @SuppressWarnings("deprecation")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.layout.activity_settings);
+
+
+    }
+}
