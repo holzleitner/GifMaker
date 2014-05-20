9c27a521d7d5120d99626fc48999138f4a5057ed
Max Holzleitner <max.holzleitner@instant-it.at>
2014-05-20 10:15:54 +0200

    working on gif view

--- GifMaker/src/main/java/at/tripwire/gifmaker/activity/GifActivity.java
+++ GifMaker/src/main/java/at/tripwire/gifmaker/activity/GifActivity.java
@@ -1,38 +1,39 @@
 package at.tripwire.gifmaker.activity;
 
+import android.graphics.Movie;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import java.io.FileOutputStream;
 
 import at.tripwire.gifmaker.R;
+import at.tripwire.gifmaker.view.GifMovieView;
 import at.tripwire.gifmaker.view.GifView;
 
 public class GifActivity extends ActionBarActivity {
+
+    private RelativeLayout gifLayout;
 
     private Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_gif);
+        setContentView(R.layout.activity_gif);
 
         byte[] gif = getIntent().getByteArrayExtra("data");
-        setContentView(new GifView(this, gif));
+
+        GifMovieView view = new GifMovieView(this);
+        view.setMovie(Movie.decodeByteArray(gif, 0, gif.length));
+
+        gifLayout = (RelativeLayout) findViewById(R.id.gifLayout);
+        gifLayout.addView(view);
 
         //writeFile(gif);
-
-        saveButton = (Button) findViewById(R.id.save_button);
-
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
     }
 
     private void writeFile(byte[] gif) {
--- GifMaker/src/main/java/at/tripwire/gifmaker/view/GIFView.java
+++ GifMaker/src/main/java/at/tripwire/gifmaker/view/GIFView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Movie;
+import android.os.SystemClock;
 import android.view.View;
 
 public class GifView extends View {
@@ -17,18 +18,40 @@ public class GifView extends View {
         movie = Movie.decodeByteArray(gif, 0, gif.length);
     }
 
+    /*
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawColor(Color.WHITE);
-        long now = android.os.SystemClock.uptimeMillis();
-        if (start == 0) { // first time
+        //canvas.drawColor(Color.WHITE);
+        if (movie != null) {
+            long now = android.os.SystemClock.uptimeMillis();
+            if (start == 0) { // first time
+                start = now;
+            }
+            int relTime = (int) ((now - start) % movie.duration());
+            movie.setTime(relTime);
+            movie.draw(canvas, this.getWidth() / 2 - 20, this.getHeight() / 2 - 40);
+            this.invalidate();
+        }
+    }
+
+    */
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.TRANSPARENT);
+        super.onDraw(canvas);
+        final long now = SystemClock.uptimeMillis();
+
+        if (start == 0) {
             start = now;
         }
+
         int relTime = (int) ((now - start) % movie.duration());
         movie.setTime(relTime);
-        movie.draw(canvas, this.getWidth() / 2 - 20, this.getHeight() / 2 - 40);
+        movie.draw(canvas, 10, 10);
         this.invalidate();
     }
 }
